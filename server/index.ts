55d4851f3a7c2ac3e75cc111fc7aae7350efd33f
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2023-02-25 13:26:15 -0500

    Update schedule

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 const events = [
-    { name: "1 on 1 Tournament", date: "March 11, 2023", time: "1:00PM - 6:00PM", num_people: 12, location: "King's Court Etobicoke - 1589 The Queensway, Etobicoke, ON M8Z 1V1", link: "https://forms.gle/7pig9exCbKrYocgK6", description: ["1 on 1 ball tournament with a 5 on 5 run after.", "Round robin followed by BO3 single elimination."] },
+    { name: "1 on 1 Tournament", date: "March 11, 2023", time: "1:00PM - 4:00PM", num_people: 12, location: "King's Court Etobicoke - 1589 The Queensway, Etobicoke, ON M8Z 1V1", link: "https://forms.gle/7pig9exCbKrYocgK6", description: ["1 on 1 ball tournament with a 5 on 5 scrimmage after.", "1 on 1 Tournament: 1 - 2PM", "Full Court Scrimmage: 2-4PM"] },
 ];
 
 app.get("/api/events", (req: Request, res: Response) => {
